45d7010d91acf5ad1a89381689a4afa0d519e60c
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-09-08 01:48:47 +0300

    Update PlayerInput.cs

--- Assets/Resources/Scripts/Input/PlayerInput.cs
+++ Assets/Resources/Scripts/Input/PlayerInput.cs
@@ -4,5 +4,6 @@ public static class PlayerInput
 {
     public static float VerticalAxis { get => Input.GetAxisRaw("Vertical"); }
     public static float HorizontalAxis { get => Input.GetAxisRaw("Horizontal"); }
+    public static Vector3 MovementVector { get => new Vector3(HorizontalAxis, 0, VerticalAxis).normalized; }
     public static bool IsDashPressed { get => Input.GetMouseButtonDown(0); }
 }
