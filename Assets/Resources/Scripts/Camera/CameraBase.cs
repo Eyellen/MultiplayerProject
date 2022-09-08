dff9850608455d5aab0e29386b7b9eb8a8afc8a1
Eyellen <83500523+Eyellen@users.noreply.github.com>
2022-09-08 13:23:32 +0300

    Create CameraBase.cs

---
+++ Assets/Resources/Scripts/Camera/CameraBase.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using Mirror;
+
+public class CameraBase : NetworkBehaviour
+{
+#if UNITY_EDITOR || DEBUG_BUILD
+    [SerializeField]
+    protected bool _debugging = false;
+#endif
+
+    protected Transform _transform;
+
+    [SerializeField]
+    protected Vector2 _sensitivity = new Vector2(1, 1);
+
+    [field: SerializeField]
+    protected float yMaxRotation { get; set; } = -90;
+    [field: SerializeField]
+    protected float yMinRotation { get; set; } = 90;
+
+    protected float XRotation { get; set; }
+    protected float YRotation { get; set; }
+
+    protected virtual void Awake()
+    {
+        XRotation = transform.rotation.eulerAngles.y;
+        YRotation = -transform.rotation.eulerAngles.x;
+
+        _transform = GetComponent<Transform>();
+    }
+
+    protected virtual void LateUpdate()
+    {
+        HandleRotation();
+    }
+
+    protected virtual void HandleRotation()
+    {
+        XRotation += PlayerInput.MouseHorizontalAxis * _sensitivity.x;
+        YRotation += PlayerInput.MouseVerticalAxis * _sensitivity.y;
+
+        YRotation = Mathf.Clamp(YRotation, yMaxRotation, yMinRotation);
+        Quaternion rotation = Quaternion.Euler(-YRotation, XRotation, 0);
+
+        _transform.rotation = rotation;
+    }
+}
